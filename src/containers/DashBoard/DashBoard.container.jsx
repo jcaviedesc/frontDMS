d575e1fbf1bafbe9e05f4dbcceb7af688e4dcaab
jcamilo97 <jcamilo97@gmail.com>
2018-11-19 09:51:59 -0500

    dashboard page

--- src/containers/DashBoard/DashBoard.container.jsx
+++ src/containers/DashBoard/DashBoard.container.jsx
@@ -1,24 +1,69 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 // componets
 import { Card } from '../../components/card';
 
-class Dashboard extends React.Component{
-  
-  render(){
-    return(
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.handlePushRouter = this.handlePushRouter.bind(this)
+  }
+
+  handlePushRouter = (target) => {
+    console.log("dashboiard - handlePushRouter",target)
+    const { history } = this.props;
+    history.push(target)
+  };
+
+  render() {
+    return (
       <div className="Dashboard">
         {/* <div> */}
-            <Card>
+
+        <Card>
+          <button id="register" className="Card-button" onClick={()=>this.handlePushRouter("/register")}>
+            <div className="Container column">
+              <div className="Container-img">
+                <img alt="usr" src="http://kinecorder.com/wp-content/uploads/2017/06/avatar_male.png" />
+              </div>
+
+              <div className="Container column Container-Text">
                 <span>Registrar usuario</span>
-            </Card>
-            <Card>
+              </div>
+            </div>
+          </button>
+        </Card>
+
+        <Card>
+          <button className="Card-button" onClick={()=>this.handlePushRouter("/document")}>
+            <div className="Container column">
+              <div className="Container-img">
+                <img alt="doc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDQu-YxE2MHz_C0frRbPvNl8ZuNy8mH1-JWTDyDIuCbbtQRley" />
+              </div>
+
+              <div className="Container column Container-Text">
                 <span>Radicar Documento</span>
-            </Card>
+              </div>
+            </div>
+          </button>
+        </Card>
         {/* </div> */}
       </div>
     );
   }
 }
 
-export { Dashboard as DashboardPage};
+/* Container */
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = dispatch => ({
+  //login: (email, password) => dispatch(appActions.login(email, password))
+})
+
+const DashboardContainer = connect(mapStateToProps, mapDispatchToProps)(Dashboard)
+export { DashboardContainer as DashboardPage };
